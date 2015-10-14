16b1644bc25c1afe56ae08cbe1594edea879253b
Maxime Lapointe <maxx@themaxx.ca>
2015-10-14 14:58:44 -0400

    wip

--- lapare.ca/lapare/apps/www/views.py
+++ lapare.ca/lapare/apps/www/views.py
@@ -5,4 +5,4 @@ from django.template import RequestContext
 def site_root(request):
     context_dict = {}
     context = RequestContext(request)
-    return render_to_response('base.html', context_dict, context)
+    return render_to_response('index.html', context_dict, context)
